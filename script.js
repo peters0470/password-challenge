090c078f4e6ea472c3b7450342cdc75fc9203eae
unknown <peters0470@gmail.com>
2020-10-10 14:26:32 -0500

    finishing touches

--- script.js
+++ script.js
@@ -4,22 +4,28 @@ regAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", "/", "+" ];
 
-var length;
+var lengths;
 var chooseCapital;
 var chooseLower;
 var chooseNumber;
 var chooseSymbol;
-var selection;
+var selection = [];
 
+function getRandomChar(arr) {
+  var randomIndex = Math.floor(Math.random() * arr.length);
+  var randomChar = arr[randomIndex];
+  return randomChar;
+};
 
 function generatePassword() {
- length = parseInt(window.prompt("Please pick a password length between 8 and 128 characters."));
- if (!length) {
+var result = [];
+ lengths = parseInt(window.prompt("Please pick a password length between 8 and 128 characters."));
+ if (!lengths) {
    window.alert("Please pick a valid value!");
     return generatePassword();
 
- } else if (length <= 8 || length >= 128) {
-   length = parseInt(window.prompt("Please choose between 8 and 128."));
+ } else if (lengths <= 8 || length >= 128) {
+   lengths = parseInt(window.prompt("Please choose between 8 and 128."));
     return generatePassword();
 
  } else {
@@ -33,52 +39,25 @@ function generatePassword() {
    selection = window.alert("You must choose at least one option!");
    return generatePassword();
  }
-
- else if (chooseCapital && chooseLower && chooseSymbol && chooseSymbol) {
-   selection = capAlphabet.concat(regAlphabet, numbers, symbols);
+ if (chooseCapital) {
+   selection = selection.concat(capAlphabet);
  }
- else if (chooseCapital) {
-   selection = capAlphabet;
+ if (chooseLower) {
+  selection = selection.concat(regAlphabet);
+}
+if (chooseNumber) {
+  selection = selection.concat(numbers);
+}
+if (chooseSymbol) {
+  selection = selection.concat(symbols);
+}
+ for (var i = 0; i < lengths; i++) {
+   var randomChar = getRandomChar(selection);
+   result.push(randomChar); 
  }
- else if (chooseLower) {
-   selection = regAlphabet;
- } 
- else if (chooseNumber) {
-   selection = numbers;
- }
- else if (chooseSymbol) {
-   selection = symbols;
- }
- else if (chooseCapital && chooseLower) {
-   selection = capAlphabet.concat(regAlphabet);
- }
- else if (chooseCapital && chooseNumber) {
-   selection = capAlphabet.concat(numbers);
- }
- else if (chooseCapital && chooseSymbol) {
-   selection = capAlphabet.concat(symbols);
- }
- else if (chooseLower && chooseNumber) {
-   selection = regAlphabet.concat(numbers);
- }
- else if (chooseLower && chooseSymbol) {
-   selection = regAlphabet.concat(symbols);
- }
- else if (chooseNumber && chooseSymbol) {
-   selection = numbers.concat(symbols);
- }
- else if (chooseCapital && chooseLower && chooseNumber) {
-   selection = capAlphabet.concat(regAlphabet, numbers);
- }
- else if (choooseCapital && chooseLower && chooseSymbol) {
-   selection = capAlphabet.concat(regAlphabet, symbols);
- }
- else if (chooseCapital && chooseNumber && chooseSymbol) {
-   selection = capAlphabet.concat(numbers, symbols);
- }
- else if (chooseLower && chooseNumber && chooseSymbol) {
-   selection = regAlphabet.concat(numbers, symbols);
- }
+ console.log(selection);
+ selection = [];
+ return result.join("");
 };
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
