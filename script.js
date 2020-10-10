d9225bfc1085bdac0573d5afc8b4976f2133c57d
unknown <peters0470@gmail.com>
2020-10-10 07:27:36 -0500

    added potential variables and first window prompts

--- script.js
+++ script.js
@@ -3,6 +3,24 @@ capAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
 regAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", "/", "+" ];
+
+var length;
+var chooseCapital;
+var chooseLower;
+var chooseNumber;
+var chooseSymbol;
+var selection;
+
+
+function generatePassword() {
+ length = parseInt(window.prompt("Please pick a password length between 8 and 128 characters."));
+ if (!length) {
+   window.alert("Please pick a valid value!");
+ }else if (length <= 8 || enter >= 128) {
+   length = parseInt(window.prompt("Please choose between 8 and 128."));
+
+ }
+};
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
