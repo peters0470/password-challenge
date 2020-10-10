68328f17e02826856bbdbf032af999b431a14f6d
unknown <peters0470@gmail.com>
2020-10-10 09:45:41 -0500

    added potential variables

--- script.js
+++ script.js
@@ -16,15 +16,62 @@ function generatePassword() {
  length = parseInt(window.prompt("Please pick a password length between 8 and 128 characters."));
  if (!length) {
    window.alert("Please pick a valid value!");
- }else if (length <= 8 || enter >= 128) {
-   length = parseInt(window.prompt("Please choose between 8 and 128."));
+    return generatePassword();
 
+ } else if (length <= 8 || length >= 128) {
+   length = parseInt(window.prompt("Please choose between 8 and 128."));
+    return generatePassword();
+
+ } else {
+   chooseCapital = window.confirm("Do you want to include capital letters?");
+   chooseLower = window.confirm("Do you want to include lowercase letters?");
+   chooseNumber = window.confirm("Do you want to include numbers?");
+   chooseSymbol = window.confirm("Do you want to include symbols?");
+ };
+
+ if (!chooseCapital && !chooseLower && !chooseNumber && !chooseSymbol) {
+   selection = window.alert("You must choose at least one option!");
+   return generatePassword();
+ }
+
+ else if (chooseCapital && chooseLower && chooseSymbol && chooseSymbol) {
+   selection = capAlphabet.concat(regAlphabet, numbers, symbols);
+ }
+ else if (chooseCapital) {
+   selection = capAlphabet;
+ }
+ else if (chooseLower) {
+   selection = regAlphabet;
+ } 
+ else if (chooseNumber) {
+   selection = numbers;
+ }
+ else if (chooseSymbol) {
+   selection = symbols;
+ }
+ else if (chooseCapital && chooseLower) {
+   selection = capAlphabet.concat(regAlphabet);
+ }
+ else if (chooseCapital && chooseNumber) {
+   selection = capAlphabet.concat(numbers);
+ }
+ else if (chooseCapital && chooseSymbol) {
+   selection = capAlphabet.concat(symbols);
+ }
+ else if (chooseLower && chooseNumber) {
+   selection = regAlphabet.concat(numbers);
+ }
+ else if (chooseLower && chooseSymbol) {
+   selection = regAlphabet.concat(symbols);
+ }
+ else if (chooseNumber && chooseSymbol) {
+   selection = numbers.concat(symbols);
  }
 };
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Write password to the #password
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
